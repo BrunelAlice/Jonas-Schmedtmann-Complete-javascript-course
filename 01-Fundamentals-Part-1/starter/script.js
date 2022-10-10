aa688584596048c89cefde76a921097b82c5848e
Brunel Alice <alc.brunel@gmail.com>
2022-10-10 15:32:40 +0100

    Answered section questions

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -36,7 +36,9 @@
         console.log(typeof language);
         
         
-
-
-        
+        console.log('1. If your country split in half, and each half would contain half the population, then how many people would live in each half? Answer: ', population / 2);
+        console.log('2. Someone is born! Now the population is: ', population + 1);
+        console.log('3. Finland has a population of 6 million. Does your country have more people than Finland? Answer: ', population > 60000000);
+        console.log('The average population of a country is 33 million people. Does your country have less people than the average country? ', population < 33000000);
+        console.log(country + ' is in ' + continent + ' and its ' + population + ' people speak ' + language)
 
