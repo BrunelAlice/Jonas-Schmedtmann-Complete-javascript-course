3bd26d6c64ba19c425b74e967f7f1ea0133dc60b
Brunel Alice <alc.brunel@gmail.com>
2022-10-10 14:04:18 +0100

    Created 3 variables and log the values to the console

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,2 +1,9 @@
         let js = 'amazing';
         if (js === 'amazing') alert('JavaScript is FUN!');
+
+        let country = 'United Kingdom';
+        let continent = 'Europe';
+        let population = '68693101';
+        console.log(country);
+        console.log(continent);
+        console.log(population);
