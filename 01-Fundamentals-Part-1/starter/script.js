f741089257d4636cc022ced3dd2616de27a57e89
Brunel Alice <alc.brunel@gmail.com>
2022-10-10 14:58:30 +0100

    Value type verification with console log

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,9 +1,28 @@
-        let js = 'amazing';
+        /* let js = 'amazing';
         if (js === 'amazing') alert('JavaScript is FUN!');
+        */
 
         let country = 'United Kingdom';
         let continent = 'Europe';
-        let population = '68693101';
+        let population = 68693101;
+
+        /*
         console.log(country);
         console.log(continent);
         console.log(population);
+        */
+
+        // Use let to declare
+        // Use the variable name to change it 
+
+        let javascriptIsFun = true;
+        //console.log(typeof javascriptIsFun);
+        javascriptIsFun = 'YES!';
+        //console.log('The type is '+ typeof javascriptIsFun);
+
+        let isIsland = true;
+        let language;
+        console.log(typeof isIsland);
+        console.log(typeof country);
+        console.log(typeof population);
+        console.log(typeof language);
