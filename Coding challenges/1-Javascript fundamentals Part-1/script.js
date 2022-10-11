ed13c837eeb816a64ef9790fab02f60e55da04f8
Brunel Alice <alc.brunel@gmail.com>
2022-10-11 12:06:49 +0100

    My solution

--- Coding challenges/1-Javascript fundamentals Part-1/script.js
+++ Coding challenges/1-Javascript fundamentals Part-1/script.js
@@ -20,7 +20,28 @@ whether Mark has a higher BMI than John.
         m tall.
 */
 
-const markMass = 78;
-const johnMass = 92;
-const markHeight = 1.69;
-const johnHeight = 1.88;
+// First data set
+let markMass = 78;
+let johnMass = 92;
+let markHeight = 1.69;
+let johnHeight = 1.88;
+
+let markBMI = markMass / markHeight ** 2;
+let johnBMI = johnMass / johnHeight ** 2;
+
+let markHigherBMI = markBMI > johnBMI;
+
+console.log('Mark ', markBMI, 'John ', johnBMI, markHigherBMI);
+
+// Second data set
+markMass = 95;
+johnMass = 85;
+markHeight = 1.88;
+johnHeight = 1.76;
+
+markBMI = markMass / markHeight ** 2;
+johnBMI = johnMass / johnHeight ** 2;
+
+markHigherBMI = markBMI > johnBMI;
+
+console.log('Mark ', markBMI, 'John ', johnBMI, markHigherBMI);
