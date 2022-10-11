cb3955f6375170e45efc2d418b8248e1ca2d945d
Brunel Alice <alc.brunel@gmail.com>
2022-10-11 13:50:09 +0100

    Printed out answer using template literal

---
+++ Coding challenges/2-Javascript fundamentals Part-1/script.js
@@ -0,0 +1,58 @@
+
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+and height in meter).
+
+Your tasks:
+
+    1. Store Mark's and John's mass and height in variables
+    2. Calculate both their BMIs using the formula (you can even implement both
+versions)
+    3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+
+        Test data:
+            § Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+        m tall.
+            § Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+        m tall.
+*/
+
+// First data set
+let markMass = 78;
+let johnMass = 92;
+let markHeight = 1.69;
+let johnHeight = 1.95;
+
+let markBMI = markMass / markHeight ** 2;
+let johnBMI = johnMass / johnHeight ** 2;
+
+let markHigherBMI = markBMI > johnBMI;
+
+console.log('Mark ', markBMI, 'John ', johnBMI, markHigherBMI);
+
+// Second data set
+markMass = 95;
+johnMass = 85;
+markHeight = 1.88;
+johnHeight = 1.76;
+
+markBMI = markMass / markHeight ** 2;
+johnBMI = johnMass / johnHeight ** 2;
+
+markHigherBMI = markBMI > johnBMI;
+
+console.log('Mark ', markBMI, 'John ', johnBMI, markHigherBMI);
+
+/* ABOUT USING CONST OR NOT
+ I choose to use let to be able to send each data set to the console log, but in the correction Jonas used constants to store the data and simply commented out the code to  reduce it as you don't need to repeat calculations.
+ */
+
+if (markHigherBMI == true) {
+    console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`);
+} else {
+    console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`);
+};
+
