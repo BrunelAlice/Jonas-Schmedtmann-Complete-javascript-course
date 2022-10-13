20e921bb57ea484f543cb1b7196e04c5e2d2aaa7
Brunel Alice <alc.brunel@gmail.com>
2022-10-13 10:04:34 +0100

    Completed assignement to lecture about arrays

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,6 +1,4 @@
-'use strict';
-
-
+"use strict";
 
 /*
  
@@ -19,12 +17,12 @@ const brazil = describeCountry('Brazil', '214 000 000', 'Brasilia');
 console.log(brazil);
 */
 
-// Function declarations vs. expressions 
+// Function declarations vs. expressions
 
 // Function declaration : can be used before it's declared
 function percentageOfWorld1(population) {
     return (population / 7900) * 100;
-};
+}
 
 const populationBrazil = percentageOfWorld1(214);
 const populationFrance = percentageOfWorld1(60);
@@ -42,13 +40,27 @@ const popUK = percentageOfWorld1(70);
 console.log(popBrazil, popFrance, popUK);
 
 // Arrow functions : shorter, has no 'this' keyword
-const percentageOfWorld3 = population => (population / 7900) * 100;
+const percentageOfWorld3 = (population) => (population / 7900) * 100;
 const brazil = percentageOfWorld3(214);
-
 
 // Function calling function
 function describePopulation(country, population) {
-    return `${country} has ${population} which is about ${percentageOfWorld1(population)}% of the world`;
-};
+    return `${country} has ${population} which is about ${percentageOfWorld1(
+        population
+    )}% of the world`;
+}
 
-console.log(describePopulation('Brazil', 214), describePopulation('France', 60), describePopulation('United Kingdom', 70));
+//console.log(describePopulation('Brazil', 214), describePopulation('France', 60), describePopulation('United Kingdom', 70));
+
+//--------------ARRAYS-------------//
+
+const populations = [214, 60, 70, 235];
+console.log(populations.length === 4);
+const percentages = [
+    percentageOfWorld1(populations[0]),
+    percentageOfWorld1(populations[1]),
+    percentageOfWorld1(populations[2]),
+    percentageOfWorld1(populations[3]),
+];
+
+console.log(percentages);
