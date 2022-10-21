afaa604620aa10ead140d7dd3cd2a5cdbcf70d65
Brunel Alice <alc.brunel@gmail.com>
2022-10-21 16:22:30 +0100

    Fixed user had to click twice on check to update highscore

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,7 +1,6 @@
 "use strict";
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector(".number").textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(secretNumber);
 
 let score = 20;
@@ -15,6 +14,16 @@ const updateScore = function (score) {
     document.querySelector(".score").textContent = score;
 };
 
+const updateHighscore = function (newScore) {
+    if (newScore > highScore) {
+        highScore = newScore;
+        document.querySelector(".highscore").textContent = highScore;
+    } else {
+        document.querySelector(".highscore").textContent = highScore;
+    }
+};
+
+
 document.querySelector(".check").addEventListener("click", function () {
     const guess = Number(document.querySelector(".guess").value);
     if (score <= 1) {
@@ -24,16 +33,26 @@ document.querySelector(".check").addEventListener("click", function () {
     } else {
         if (!guess) {
             displayMessage("Not a number");
-        } else if (guess === secretNumber) {
-            displayMessage("Correct number!");
-            highScore = document.querySelector(".score").textContent;
-            document.querySelector(".highscore").textContent = highScore;
         } else if (guess != secretNumber) {
             guess > secretNumber
                 ? displayMessage("Too high")
                 : displayMessage("Too low");
             score--;
             updateScore(score);
-        }
+        } else if (guess === secretNumber) {
+            displayMessage("Correct number!");
+            let newScore = score;
+            updateHighscore(newScore);
+            return;
+        } 
     }
 });
+
+document.querySelector(".again").addEventListener("click", function () {
+    score = 20;
+    updateScore(score);
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    console.log(secretNumber);
+    displayMessage("Start guessing...");
+});
+
